a834cd8bf54615481e19c06f8e4874736a1c6696
silver-it <alex.jin220@gmail.com>
2022-05-20 12:35:06 +0800

    feat: landing agreement section

--- components/Header.vue
+++ components/Header.vue
@@ -1,24 +1,20 @@
 <template>
-  <div class="tomisha-header">
-    <div class="tomisha-header-bar"></div>
-    <div class="tomisha-header-content">
-        <span>Login</span>
+  <div class="tomisha-header rounded-b-xl h-16 bg-white">
+    <div class="tomisha-header-bar h-1.5"></div>
+    <div class="tomisha-header-content flex justify-end p-5">
+        <span class="cursor-pointer hover:underline">Login</span>
     </div>
   </div>
 </template>
 
 <style scoped>
 .tomisha-header {
-  height: 60px;
+  box-shadow: 0px 3px 6px #00000029;
 }
 .tomisha-header-bar {
-    height: 5px;
     background: transparent linear-gradient(91deg, #319795 0%, #3182CE 100%) 0% 0% no-repeat padding-box;
 }
 .tomisha-header-content {
-    display: flex;
-    padding: 20px;
-    justify-content: end;
     color: #319795;
 }
 </style>
