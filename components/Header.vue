2a2e1ed0ded698ade26ed06a461061f2360fc44d
silver-it <alex.jin220@gmail.com>
2022-05-20 11:53:23 +0800

    feat: created header

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tomisha-header">
+    <div class="tomisha-header-bar"></div>
+    <div class="tomisha-header-content">
+        <span>Login</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tomisha-header {
+  height: 60px;
+}
+.tomisha-header-bar {
+    height: 5px;
+    background: transparent linear-gradient(91deg, #319795 0%, #3182CE 100%) 0% 0% no-repeat padding-box;
+}
+.tomisha-header-content {
+    display: flex;
+    padding: 20px;
+    justify-content: end;
+    color: #319795;
+}
+</style>
